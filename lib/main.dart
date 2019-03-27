ba7926c924410b56bcbf24b5223a4931bd34827e
Tyshan Prince <tyshantprince@gmail.com>
2019-03-27 13:34:18 -0500

    Basic Layout

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_statusbar_manager/flutter_statusbar_manager.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter_rating/flutter_rating.dart';
+
 
 void main() => runApp(new MyApp());
 
@@ -21,12 +23,11 @@ class HomeScreen extends StatelessWidget {
     return CupertinoPageScaffold(
       navigationBar: CupertinoNavigationBar(
           backgroundColor: Colors.transparent,
-          middle: Text('Tuesday'),
+          middle: Text('Sat, June 15th'),
           trailing: Icon(CupertinoIcons.person_solid)),
       child: ListView(
         children: <Widget>[
           Post(),
-          Text("Hey"),
         ],
       ),
     );
@@ -39,6 +40,7 @@ class Post extends StatelessWidget {
     return Stack(
       children: <Widget>[
         PostImages(),
+        ProfileCard(),
       ],
     );
   }
@@ -52,7 +54,7 @@ class PostImages extends StatefulWidget {
 class _PostImagesState extends State<PostImages> {
   int photoIndex = 0;
 
-  List<String> photos = ['assets/profile_pic.jpg', 'assets/wallpaper.jpg'];
+  List<String> photos = ['assets/princess2.JPG','assets/princess.gif','assets/giphy.gif', 'assets/wallpaper.jpg'];
 
   @override
   Widget build(BuildContext context) {
@@ -67,16 +69,11 @@ class _PostImagesState extends State<PostImages> {
             builder: (BuildContext context) {
               return Container(
                 width: MediaQuery.of(context).size.width,
-                margin: EdgeInsets.symmetric(horizontal: 5.0),
-                padding: EdgeInsets.symmetric(horizontal: 6.0),
-
                 decoration: BoxDecoration(color: Colors.transparent),
                 child: Container(
                   decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(25.0),
                       image: DecorationImage(
-                          image: AssetImage(i),
-                          fit: BoxFit.cover)),
+                          image: AssetImage(i), fit: BoxFit.cover)),
                 ),
               );
             },
@@ -84,5 +81,82 @@ class _PostImagesState extends State<PostImages> {
         }).toList(),
       ),
     ]));
+  }
+}
+
+class ProfileCard extends StatefulWidget {
+  @override
+  _ProfileCardState createState() => new _ProfileCardState();
+}
+
+class _ProfileCardState extends State<ProfileCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: const EdgeInsets.only(top: 500.0),
+        child: Column(children: <Widget>[
+          Opacity(
+            opacity: .75,
+            child:Card(
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                const ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: AssetImage('assets/profile_pic.jpg'),
+                      radius: 25.0,
+                    ),
+                    title: Text(
+                      'Princess\'s 2nd Birthday Celebration ',
+                    ),
+                    subtitle: Text("3:00pm to 7:00pm")),
+              ],
+            ),
+          )),
+          StarCount(),
+          ButtonBar(children: <Widget>[
+            FlatButton(child: Icon(CupertinoIcons.mail, size: 40.0,),textColor: CupertinoColors.activeBlue, onPressed: () => {},),
+            FlatButton(child: Icon(CupertinoIcons.person_add, size: 40.0,),textColor: CupertinoColors.activeGreen, onPressed: () => {},)
+
+          ],
+          alignment: MainAxisAlignment.spaceBetween,
+          )
+
+        ]));
+  }
+}
+
+class StarCount extends StatefulWidget {
+  @override
+  _StarCountState createState() => new _StarCountState();
+}
+
+class _StarCountState extends State<StarCount> {
+  double rating = 3.5;
+  int starCount = 5;
+
+  @override
+  Widget build(BuildContext context) {
+    return      new Column(
+        children: <Widget>[
+          new Padding(
+            padding: new EdgeInsets.only(
+              top: 20.0,
+            ),
+            child: new StarRating(
+              size: 25.0,
+              rating: rating,
+              color: Colors.orange,
+              borderColor: Colors.grey,
+              starCount: starCount,
+              onRatingChanged: (rating) => setState(
+                    () {
+                      this.rating = rating;
+                    },
+                  ),
+            ),
+          ),
+        ]
+    );
   }
 }
