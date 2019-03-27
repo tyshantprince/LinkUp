d609486dac2a93df631b6acbc3f75f1c94d27c2f
Tyshan Prince <tyshantprince@gmail.com>
2019-03-26 19:39:48 -0500

    Found better image carousel via flutter packges

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_statusbar_manager/flutter_statusbar_manager.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 void main() => runApp(new MyApp());
 
@@ -53,130 +54,35 @@ class _PostImagesState extends State<PostImages> {
 
   List<String> photos = ['assets/profile_pic.jpg', 'assets/wallpaper.jpg'];
 
-  void _previousImage() {
-    setState(() {
-      photoIndex = photoIndex > 0 ? photoIndex - 1 : 0;
-    });
-  }
-
-  void _nextImage() {
-    setState(() {
-      photoIndex = photoIndex < photos.length - 1 ? photoIndex + 1 : photoIndex;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Stack(
-        children: <Widget>[
-          Column(
-            children:<Widget>[
-              Container(
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(25.0),
-                    image: DecorationImage(
-                        image: AssetImage(photos[photoIndex]),
-                        fit: BoxFit.cover)),
-                height: 400.0,
-                width: 300.0,
-              ),
-              Center(
-                child: SelectedPhoto(
-                    numberOfDots: photos.length, photoIndex: photoIndex),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  RaisedButton(
-                    child: Text('Next'),
-                    onPressed: _nextImage,
-                    elevation: 5.0,
-                    color: Colors.green,
-                  ),
-                  SizedBox(width: 10.0),
-                  RaisedButton(
-                    child: Text('Prev'),
-                    onPressed: _previousImage,
-                    elevation: 5.0,
-                    color: Colors.blue,
-                  )
-                ],
-              )
-            ]
-          ),
-        ],
+    return (Column(children: <Widget>[
+      CarouselSlider(
+        enableInfiniteScroll: false,
+        initialPage: 0,
+        viewportFraction: 1.0,
+        height: MediaQuery.of(context).size.height,
+        items: photos.map((i) {
+          return Builder(
+            builder: (BuildContext context) {
+              return Container(
+                width: MediaQuery.of(context).size.width,
+                margin: EdgeInsets.symmetric(horizontal: 5.0),
+                padding: EdgeInsets.symmetric(horizontal: 6.0),
+
+                decoration: BoxDecoration(color: Colors.transparent),
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(25.0),
+                      image: DecorationImage(
+                          image: AssetImage(i),
+                          fit: BoxFit.cover)),
+                ),
+              );
+            },
+          );
+        }).toList(),
       ),
-    );
+    ]));
   }
-}
-
-class SelectedPhoto extends StatelessWidget {
-  final int numberOfDots;
-  final int photoIndex;
-
-  SelectedPhoto({this.numberOfDots, this.photoIndex});
-
-    Widget _inactivePhoto() {
-    return new Container(
-      child: new Padding(
-        padding: const EdgeInsets.only(left: 3.0, right: 3.0),
-        child: Container(
-          height: 8.0,
-          width: 8.0,
-          decoration: BoxDecoration(
-            color: Colors.grey,
-            borderRadius: BorderRadius.circular(4.0)
-          ),
-        ),
-      )
-    );
-  }
-
-  Widget _activePhoto() {
-    return Container(
-      child: Padding(
-        padding: EdgeInsets.only(left: 3.0, right: 3.0),
-        child: Container(
-          height: 10.0,
-          width: 10.0,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(5.0),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey,
-                spreadRadius: 0.0,
-                blurRadius: 2.0
-              )
-            ]
-          ),
-        ),
-      ),
-    );
-  }
-
-  List<Widget> _buildDots() {
-    List<Widget> dots = [];
-
-    for(int i = 0; i< numberOfDots; ++i) {
-      dots.add(
-        i == photoIndex ? _activePhoto(): _inactivePhoto()
-      );
-    }
-
-    return dots;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Center(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: _buildDots(),
-      ),
-    );
-  }
-
-
 }
